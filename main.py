86babf449eae6ff5d95efaf992ebc5b65d5ea8ee
Panhavoan Kymeas <kymeaspanhavoan@gmail.com>
2026-03-16 19:24:03 +0700

    add on startup event

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from routers import sentences, recordings
+from database import engine, Base
 
 app = FastAPI()
 
@@ -12,3 +13,9 @@ app.include_router(sentences.router)
 app.include_router(recordings.router)
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+@app.on_event("startup")
+async def on_startup():
+    async with engine.begin() as conn:
+        # This will create all tables defined in your models if they don't exist
+        await conn.run_sync(Base.metadata.create_all)
